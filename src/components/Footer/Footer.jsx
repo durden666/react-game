4d4912f51c2b0721f83c0ff381738571e32a62ea
durden666 <thanks.vlad12@gmail.com>
2021-03-03 20:47:48 +0300

    feat: add footer logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import logo from '../../assets/img/logo.svg'
 const Footer = () => {
   return (
     <div className="footerContainer">
       <span>2021</span>
-      <a className="account">Author's GitHub account</a>
-      <a className="logoContainer" href="https://rs.school/js/"><img className="logo" /></a>
+      <a href="https://github.com/durden666" className="account">Author's GitHub account</a>
+      <a className="logoContainer" href="https://rs.school/js/">
+        <img src={logo} className="logo" />
+      </a>
     </div>
   );
 }
